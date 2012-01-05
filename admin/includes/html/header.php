1d9a35f10a97a36a1f96a14952786f66da9d37c6
Baptouuuu <baptouuuu@gmail.com>
2012-01-05 20:34:03 +0100

    Usage of fancybox
    Plus modification of menu structure

--- admin/includes/html/header.php
+++ admin/includes/html/header.php
@@ -1,4 +1,7 @@
-<?php use \Library\Variable\Session as Session; ?>
+<?php 
+	use \Library\Variable\Session as Session;
+	use \Admin\Helper\Helper as Helper;
+?>
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -10,11 +13,20 @@
 		<link rel="index" href="<?php echo WS_URL ?>" title="<?php echo WS_NAME ?>" />
 		<link rel="icon" type="image/png" href="<?php echo PATH ?>images/lynxpress-mini.png" />
 		<link rel="stylesheet" type="text/css" href="<?php echo PATH ?>css/admin.css" />
-		<link rel="stylesheet" type="text/css" href="<?php echo PATH ?>shadowbox/shadowbox.css" />
-		<script type="text/javascript" src="<?php echo PATH ?>shadowbox/shadowbox.js"></script>
+		<link rel="stylesheet" type="text/css" href="<?php echo PATH ?>fancybox/jquery.fancybox-1.3.4.css" media="screen" />
+		
+		<script src="<?php echo PATH ?>js/jquery-1.4.3.min.js"></script>
+		<script type="text/javascript" src="<?php echo PATH ?>fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
+		<script type="text/javascript" src="<?php echo PATH ?>fancybox/jquery.fancybox-1.3.4.pack.js"></script>
 		
 		<script type="text/javascript">
-			Shadowbox.init();
+			$(document).ready(function() {
+				$("a.fancybox").fancybox({
+					'titlePosition'		: 'outside',
+					'overlayColor'		: '#000',
+					'overlayOpacity'	: 0.85
+				});
+			});
 		</script>
 
 	</head>
@@ -23,33 +35,127 @@
 
 		<header>
 		
-			<nav>
+			<ul id="nav">
 				
+				<li>
 					<a href="../" target="_blank" title="Back to your website"><?php echo WS_NAME ?></a>
-				
+				</li>
+			
+				<li>
 					<a href="index.php">Dashboard</a>
-				
+				</li>
+			
+				<li>
 					<a href="index.php?ns=posts&ctl=manage">Posts</a>
-				
-					<a href="index.php?ns=media&ctl=manage">Media</a>
-				
-					<a href="index.php?ns=comments&ctl=manage">Comments</a>
-				
-					<a href="index.php?ns=users&ctl=profile">Profile</a>
 					
+					<ul>
+						<li><a href="index.php?ns=posts&ctl=add">Add</a></li>
+						<li>
+							<a href="index.php?ns=posts&ctl=manage">Posts</a>
+							
+							<ul>
+								<li><a href="index.php?ns=posts&ctl=manage&post_status=publish">Published</a></li>
+								<li><a href="index.php?ns=posts&ctl=manage&post_status=draft">Draft</a></li>
+								<li><a href="index.php?ns=posts&ctl=manage&post_status=trash">Trash</a></li>
+							</ul>
+						</li>
+					</ul>
+				</li>
+			
+				<li>
+					<a href="index.php?ns=media&ctl=manage">Media</a>
+					
+					<ul>
+						<li>
+							<a href="index.php?ns=media&ctl=add">Add</a>
+							
+							<ul>
+								<li><a href="index.php?ns=media&ctl=add&view=album">Album</a></li>
+								<li><a href="index.php?ns=media&ctl=add&view=linkage">Linkage</a></li>
+								<li><a href="index.php?ns=media&ctl=add&view=video">Video</a></li>
+							</ul>
+						</li>
+						<li>
+							<a href="index.php?ns=media&ctl=manage">Media</a>
+							
+							<ul>
+								<li><a href="index.php?ns=media&ctl=manage">Images</a></li>
+								<li><a href="index.php?ns=media&ctl=manage&type=video">Videos</a></li>
+								<li><a href="index.php?ns=media&ctl=manage&type=alien">External Videos</a></li>
+							</ul>
+						</li>
+						<li><a href="index.php?ns=media&ctl=albums">Albums</a></li>
+					</ul>
+				</li>
+			
+				<li>
+					<a href="index.php?ns=comments&ctl=manage">Comments</a>
+					
+					<ul>
+						<li><a href="index.php?ns=comments&ctl=manage">Pending</a></li>
+						<li><a href="index.php?ns=comments&ctl=manage&comment_status=approved">Approved</a></li>
+						<li><a href="index.php?ns=comments&ctl=manage&comment_status=spam">Spam</a></li>
+						<li><a href="index.php?ns=comments&ctl=manage&comment_status=trash">Trash</a></li>
+					</ul>
+				</li>
+			
+				<li>
+					<a href="index.php?ns=users&ctl=profile">Profile</a>
+				</li>
+				
+				<li>
 					<a href="index.php?ns=plugins&ctl=bridge">Plugins</a>
+				</li>
 				
 				<?php
 				
-					if($page->settings)
-						echo '<a href="index.php?ns=settings&ctl=manage">Settings</a>';
+					if($page->settings){
+					
+						echo '<li>'.
+							 	'<a href="index.php?ns=settings&ctl=manage">Settings</a>'.
+							 	'<ul>'.
+							 		'<li><a href="index.php?ns=categories&ctl=manage">Categories</a></li>'.
+							 		'<li><a href="index.php?ns=posts&ctl=settingpage">Posts</a></li>'.
+							 		'<li>'.
+							 			'<a href="index.php?ns=users&ctl=manage">Users</a>'.
+							 			'<ul>'.
+							 				'<li><a href="index.php?ns=users&ctl=add">Add</a></li>'.
+							 			'</ul>'.
+							 		'</li>'.
+							 		'<li><a href="index.php?ns=roles&ctl=manage">Roles</a></li>'.
+							 		'<li><a href="index.php?ns=social&ctl=manage">Social</a></li>'.
+							 		'<li><a href="index.php?ns=defaultpage&ctl=manage">Default Page</a></li>'.
+							 		'<li>'.
+							 			'<a href="index.php?ns=templates&ctl=manage">Templates</a>'.
+							 			'<ul>'.
+							 				'<li><a href="index.php?ns=templates&ctl=add">Add</a></li>'.
+							 			'</ul>'.
+							 		'</li>'.
+							 		'<li>'.
+							 			'<a href="index.php?ns=plugins&ctl=manage">Plugins</a>'.
+							 			'<ul>'.
+							 				'<li><a href="index.php?ns=plugins&ctl=add">Add</a></li>'.
+							 			'</ul>'.
+							 		'</li>'.
+							 		'<li>'.
+							 			'<a href="index.php?ns=links&ctl=manage">Links</a>'.
+							 			'<ul>'.
+							 				'<li><a href="index.php?ns=links&ctl=add">Add</a></li>'.
+							 			'</ul>'.
+							 		'</li>'.
+							 		'<li><a href="index.php?ns=activity&ctl=manage">Activity</a></li>'.
+							 		'<li><a href="index.php?ns=update&ctl=manage">Update</a></li>'.
+							 	'</ul>'.
+							 '<li>';
+					
+					}
 				
 				?>
-				<span id="right">
+				<li id="right">
 					Hi <?php echo Session::username() ?> | 
 					<a href="logout.php">Logout</a>
-				</span>
-			</nav>
+				</li>
+			</ul>
 			
 		</header>
 		
