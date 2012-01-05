9fbee88d98a0577c5f8012138410506a8c293bcd
Baptouuuu <baptouuuu@gmail.com>
2012-01-05 20:56:46 +0100

    log message improved

--- includes/class.author.inc.php
+++ includes/class.author.inc.php
@@ -111,7 +111,7 @@
 			
 			}catch(Exception $e){
 			
-				@error_log($e->getMessage(), 1, WS_EMAIL);
+				@error_log($e->getMessage().' file: '.__FILE__.'; line: '.__LINE__, 1, WS_EMAIL);
 				header('Location: 404.php');
 			
 			}
