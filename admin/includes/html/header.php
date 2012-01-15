36a1c2e58eeea62169e90029d3fad7cfcd49e2ae
Baptouuuu <baptouuuu@gmail.com>
2012-01-15 20:46:11 +0100

    Update fancybox from 1.3.4 to 2.0.4

--- admin/includes/html/header.php
+++ admin/includes/html/header.php
@@ -1,6 +1,7 @@
 <?php 
 	use \Library\Variable\Session as Session;
 	use \Admin\Helper\Helper as Helper;
+	use \Library\Variable\Get as VGet;
 ?>
 <!DOCTYPE html>
 
@@ -13,18 +14,32 @@
 		<link rel="index" href="<?php echo WS_URL ?>" title="<?php echo WS_NAME ?>" />
 		<link rel="icon" type="image/png" href="<?php echo PATH ?>images/lynxpress-mini.png" />
 		<link rel="stylesheet" type="text/css" href="<?php echo PATH ?>css/admin.css" />
-		<link rel="stylesheet" type="text/css" href="<?php echo PATH ?>fancybox/jquery.fancybox-1.3.4.css" media="screen" />
+		<link rel="stylesheet" type="text/css" href="<?php echo PATH ?>fancybox/jquery.fancybox.css" media="screen" />
+		<link rel="stylesheet" type="text/css" href="<?php echo PATH ?>fancybox/helpers/jquery.fancybox-buttons.css?v=2.0.3" />
+		<?php
 		
-		<script src="<?php echo PATH ?>js/jquery-1.4.3.min.js"></script>
-		<script type="text/javascript" src="<?php echo PATH ?>fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
-		<script type="text/javascript" src="<?php echo PATH ?>fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+			if(file_exists(PATH.'css/'.VGet::ns().'.css'))
+				echo '<link rel="stylesheet" type="text/css" href="'.PATH.'css/'.VGet::ns().'.css" />';
+		
+		?>
+		
+		<script type="text/javascript" src="<?php echo PATH ?>js/jquery-1.7.1.min.js"></script>
+		<script type="text/javascript" src="<?php echo PATH ?>js/jquery.mousewheel-3.0.6.pack.js"></script>
+		<script type="text/javascript" src="<?php echo PATH ?>fancybox/jquery.fancybox.js"></script>
+		<script type="text/javascript" src="<?php echo PATH ?>fancybox/helpers/jquery.fancybox-buttons.js?v=2.0.3"></script>
 		
 		<script type="text/javascript">
 			$(document).ready(function() {
 				$("a.fancybox").fancybox({
-					'titlePosition'		: 'outside',
-					'overlayColor'		: '#000',
-					'overlayOpacity'	: 0.85
+					helpers: {
+						title : {
+							type : 'outside'
+						},
+						overlay : {
+							speedIn : 500,
+							opacity : 0.85
+						}
+					}
 				});
 			});
 		</script>
@@ -105,6 +120,24 @@
 				
 				<li>
 					<a href="index.php?ns=plugins&ctl=bridge">Plugins</a>
+					
+					<?php 
+					
+						$plugins = Helper::plugins_infos();
+						
+						if(!empty($plugins)){
+						
+							echo '<ul>';
+							
+							foreach($plugins as $plg)
+								echo '<li><a href="index.php?ns='.$plg['namespace'].'&ctl='.$plg['entry_point'].'">'.$plg['name'].'</a></li>';
+							
+							echo '</ul>';
+						
+						}
+					
+					?>
+					
 				</li>
 				
 				<?php
@@ -129,12 +162,14 @@
 							 			'<a href="index.php?ns=templates&ctl=manage">Templates</a>'.
 							 			'<ul>'.
 							 				'<li><a href="index.php?ns=templates&ctl=add">Add</a></li>'.
+							 				'<li><a href="index.php?ns=templates&ctl=library">Library</a></li>'.
 							 			'</ul>'.
 							 		'</li>'.
 							 		'<li>'.
 							 			'<a href="index.php?ns=plugins&ctl=manage">Plugins</a>'.
 							 			'<ul>'.
 							 				'<li><a href="index.php?ns=plugins&ctl=add">Add</a></li>'.
+							 				'<li><a href="index.php?ns=plugins&ctl=library">Library</a></li>'.
 							 			'</ul>'.
 							 		'</li>'.
 							 		'<li>'.
@@ -153,7 +188,7 @@
 				?>
 				<li id="right">
 					Hi <?php echo Session::username() ?> | 
-					<a href="logout.php">Logout</a>
+					<a href="index.php?ns=session&ctl=logout">Logout</a>
 				</li>
 			</ul>
 			
